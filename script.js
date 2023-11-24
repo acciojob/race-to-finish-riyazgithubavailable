ce71421d497a13c343b0b6bce8a908aee4110c1f
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2023-11-24 12:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 window.promises = [];
-
+Promise.any(promises)
+.then(result => {
+	document.getElementById('output').innerText=result;
+})
+.catch(err =>{
+	console.log(err);
+})
 // Do not change the code above this
 // add your promises to the array `promises`
+
+
+
