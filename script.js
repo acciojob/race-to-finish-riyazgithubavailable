8422b7646915b819fe8c659bf415075404dd4b08
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2023-11-24 12:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.promises = [];
+        function getRandomTime(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min) * 1000; // Convert to milliseconds
+        }
+
         const promises = [
             new Promise(resolve => setTimeout(() => resolve("Promise 1 resolved"), getRandomTime(1, 5))),
             new Promise(resolve => setTimeout(() => resolve("Promise 2 resolved"), getRandomTime(1, 5))),
